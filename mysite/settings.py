f80196918ee1887279072c7b971120dc9f894382
brad-eshares <brad@esharesinc.com>
2013-04-15 22:08:56 -0700

    Trucking

--- mysite/settings.py
+++ mysite/settings.py
@@ -111,6 +111,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+
+    '/Users/BKN/Dropbox/test_envs/djangoproject1/mysite/templates'
 )
 
 INSTALLED_APPS = (
@@ -121,7 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'polls',
--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
 # Create your models here.
 
@@ -9,6 +11,11 @@ class Poll(models.Model):
     pub_date = models.DateTimeField('date published')
     def __unicode__(self):
         return self.question
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
